8815a9f72769705d6543e7a5f72eb0b53b0bc509
João Kitajima <joao.kitajima@outlook.com>
2022-07-29 02:22:31 -0300

    prj

--- themispy/project/__init__.py
+++ themispy/project/__init__.py
@@ -1,12 +1,11 @@
 from themispy.project.utils import (
     PROJECT_PATH, PROJECT_TITLE, 
-    build_path, split_url, get_files
+    build_path, split_filepath
 )
 
 __all__ = [
     "PROJECT_PATH",
     "PROJECT_TITLE",
     "build_path",
-    "split_url",
-    "get_files"
+    "split_filepath"
 ]
